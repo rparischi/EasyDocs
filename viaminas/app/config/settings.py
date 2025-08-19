3148090eb9f6fae54c15d44823b41d28d707b162
rparischi <88691005+rparischi@users.noreply.github.com>
2025-08-19 17:11:40 -0300

    Delete viaminas/app directory

--- viaminas/app/config/settings.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    "host": "129.148.43.21",
-    "user": "softlog_bsb2",
-    "password": "softlog#321",
-    "database": "softlog_bsb2"
-}
--- viaminas/app/domain/entities/canhoto.py
+++
@@ -1,8 +0,0 @@
-class Canhoto:
-    def __init__(self, numeronf, serienf, cnpjemissor, dataentrega, urlimagem, urlimagemprevia):
-        self.numeronf = numeronf
-        self.serienf = serienf
-        self.cnpjemissor = cnpjemissor
-        self.dataentrega = dataentrega
-        self.urlimagem = urlimagem
-        self.urlimagemprevia = urlimagemprevia
